aa02a3b9bb08aa3864a86c241133862ba9db465d
Eslam Mahgoub <eslamkhaledtawfik@gmail.com>
2024-02-09 11:01:53 +0100

    Added statifiles for django stroages DO spaces

--- web/django_k8s/settings.py
+++ web/django_k8s/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
+    'boto3'
 ]
 
 MIDDLEWARE = [
@@ -154,8 +156,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "staticfiles"]
+STATIC_ROOT = BASE_DIR / "staticfiles-cdn"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from .cdn.conf import * #noqa
